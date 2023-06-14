33a694a30d493af50208001aaf197e4e03ed3d9b
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-06-14 16:59:40 +0100

    🌍🔐 Updated Map Component and Added Login Component

---
+++ src/scenes/Login/index.tsx
@@ -0,0 +1,152 @@
+import { Container } from "@mui/material";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  const data = new FormData(event.currentTarget);
+  console.log({
+    email: data.get("email"),
+    password: data.get("password"),
+  });
+};
+
+const Login = () => {
+  return (
+    <Container maxWidth="lg" sx={{ paddingTop: "30px" }}>
+      <Grid
+        container
+        component="main"
+        sx={{
+          height: "auto",
+          borderRadius: "20px",
+          justify: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        <Grid item xs={12} sm={12} md={6}>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "20px",
+              backgroundColor: "white",
+              borderRadius: "20px",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          display={{ xs: "none", sm: "none", md: "block" }}
+          md={6}
+          sx={{
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "/assets/login_ifrc.jpeg"}
+            alt="ifrc_image"
+            style={{
+              width: "80%",
+              height: "auto",
+              borderRadius: "20px",
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Login;
