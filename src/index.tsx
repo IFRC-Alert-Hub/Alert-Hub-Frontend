4a19d8e81135f5dfeccddc66f368789762859b2b
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-06-24 08:19:52 +0100

    🎨 Added GraphQL Client; Attempted to make a API Request

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,23 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZ28taWZyYyIsImEiOiJja3E2bGdvb3QwaXM5MnZtbXN2eGtmaWgwIn0.llipq3Spc_PPA2bLjPwIPQ";
 
+const client = new ApolloClient({
+  uri: "https://cap-aggregator.azurewebsites.net/graphql",
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
