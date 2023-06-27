99476afe11316bb917efc5a34dfe425d36700dc9
Lee <yizhouli1822220@gmail.com>
2023-06-27 23:53:40 +0100

    :sparkles: Added subscription modal form

--- src/scenes/Subscription/AddModal.tsx
+++ src/scenes/Subscription/AddModal.tsx
@@ -1,12 +1,15 @@
 import { Box, Modal, Typography } from "@mui/material";
+import ModalForm from "./ModalForm";
 
 const style = {
   position: "absolute" as "absolute",
-  top: "30%",
+  top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "40%",
   minWidth: "350px",
+  maxHeight: "500px",
+  overflow: "auto",
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
@@ -22,10 +25,10 @@ const AddModal = ({ open, handleClose }: PropsType) => {
   return (
     <Modal open={open} onClose={handleClose} aria-labelledby="modal-title">
       <Box sx={style}>
-        <Typography id="modal-title" variant="h6" fontWeight={"bold"}>
+        <Typography id="modal-title" variant="h3" fontWeight={"bold"} mb="5px">
           Add New Subscription
         </Typography>
-        <Box>form content</Box>
+        <ModalForm open={open} handleClose={handleClose} />
       </Box>
     </Modal>
   );
---
+++ src/scenes/Subscription/FormCheckbox.tsx
@@ -0,0 +1,56 @@
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+} from "@mui/material";
+import { useState } from "react";
+
+interface CheckboxProps {
+  legend: string;
+  checkboxNames: string[];
+}
+
+interface CheckboxItem {
+  [key: string]: boolean;
+}
+
+const FormCheckbox = ({ legend, checkboxNames }: CheckboxProps) => {
+  const initialState: CheckboxItem = checkboxNames.reduce((obj, value) => {
+    obj[value] = false;
+    return obj;
+  }, {} as CheckboxItem);
+
+  const [checkedItems, setCheckedItems] = useState<CheckboxItem>(initialState);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckedItems({
+      ...checkedItems,
+      [e.target.name]: e.target.checked,
+    });
+  };
+
+  return (
+    <div>
+      <FormControl component="fieldset">
+        <legend className="subs-form-legend">{legend}</legend>
+        <FormGroup sx={{ flexDirection: "row", marginLeft: 1 }}>
+          {checkboxNames.map((name) => (
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={checkedItems[name]}
+                  onChange={handleChange}
+                  name={name}
+                />
+              }
+              label={name}
+            />
+          ))}
+        </FormGroup>
+      </FormControl>
+    </div>
+  );
+};
+
+export default FormCheckbox;
