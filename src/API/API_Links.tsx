0728ea6d72d874709b36d95c486d7098089fc57c
Lee <yizhouli1822220@gmail.com>
2023-07-05 02:20:53 +0100

    :zap: Added subscription  apollo client

--- src/API/API_Links.tsx
+++ src/API/API_Links.tsx
@@ -9,3 +9,8 @@ export const auth_system = new ApolloClient({
   uri: "https://backend-deploy-dev-yx.azurewebsites.net/users/graphql",
   cache: new InMemoryCache(),
 });
+
+export const subscription_module = new ApolloClient({
+  uri: "https://backend-deploy.azurewebsites.net/subscription/graphql",
+  cache: new InMemoryCache(),
+});
