bca51b136c49893eaa1c3929e195d26000df11ce
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-07-14 09:54:57 +0100

    Alerts are now filtered within the MapComponentWithFilter

--- src/components/MapComponent/MapComponentWithFilter.tsx
+++ src/components/MapComponent/MapComponentWithFilter.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useRef, useState } from "react";
 import {
   Autocomplete,
   Box,
@@ -5,12 +6,11 @@ import {
   Container,
   TextField,
 } from "@mui/material";
-import MapComponent, { AlertData, AlertInfoSet, Bbox } from "./MapComponent";
-import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 
 import TitleHeader from "../TitleHeader";
 import DatePickerComponent from "../DatePicker/DatePicker";
+import MapComponent, { AlertData, AlertInfoSet, Bbox } from "./MapComponent";
 
 interface MapComponentWithFilterProps {
   loading: boolean;
@@ -29,50 +29,77 @@ const MapComponentWithFilter: React.FC<MapComponentWithFilterProps> = ({
   filterKey,
   selectedFilter,
 }) => {
-  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
   const mapRef = useRef<mapboxgl.Map | null>(null);
 
+  const originalAlerts = useRef<AlertData[] | null>(null);
   const [filteredAlerts, setFilteredAlerts] = useState<AlertData[]>(
     data?.listAlert || []
   );
-  console.log("filteredAlerts 2", filteredAlerts);
-  const [selectedUrgency, setSelectedUrgency] = useState("");
-  const [selectedSeverity, setSelectedSeverity] = useState("");
+  const [selectedUrgency, setSelectedUrgency] = useState<string>("");
+  const [selectedSeverity, setSelectedSeverity] = useState<string>("");
   const [alertsLoading, setAlertsLoading] = useState<boolean>(true);
-
   const [selectedEffectiveDate, setSelectedEffectiveDate] = useState<
     [number | null, number | null] | undefined
   >([null, null]);
 
-  const handleUrgencyChange = (event: any, value: any) => {
+  const handleUrgencyChange = (
+    event: React.ChangeEvent<{}>,
+    value: string | null
+  ) => {
     setSelectedUrgency(value || "");
   };
 
-  const handleSeverityChange = (event: any, value: any) => {
+  const handleSeverityChange = (
+    event: React.ChangeEvent<{}>,
+    value: string | null
+  ) => {
     setSelectedSeverity(value || "");
   };
 
-  const urgencyOptions = ["Future", "Past", "Unknown", "Immediate"];
-  const severityOptions = ["Moderate", "Minor", "Unknown", "Severe"];
+  const urgencyOptions: string[] = ["Future", "Past", "Unknown", "Immediate"];
+  const severityOptions: string[] = ["Moderate", "Minor", "Unknown", "Severe"];
 
   useEffect(() => {
-    if (!loading && !error) {
-      let filteredData = data?.listAlert || [];
+    if (!loading && !error && data) {
+      originalAlerts.current = (data?.listAlert || []).map(
+        (alert: AlertData) => {
+          const newCountry = data?.listCountry?.find(
+            (country: any) => country?.id === alert?.country?.id
+          );
+          const countryPolygon =
+            newCountry?.multipolygon === ""
+              ? JSON.parse(newCountry?.polygon || "[]")
+              : JSON.parse(newCountry?.multipolygon || "[]");
+          const type =
+            alert?.country?.multipolygon === "Polygon"
+              ? "Polygon"
+              : "MultiPolygon";
+          const updatedCountry = { ...newCountry, countryPolygon, type };
+          return { ...alert, country: updatedCountry };
+        }
+      );
+    }
+  }, [error, loading, data]);
+
+  useEffect(() => {
+    if (!loading && !error && originalAlerts.current) {
+      let filteredData = originalAlerts.current || [];
       if (selectedUrgency !== "") {
         filteredData = filteredData.filter((alert: AlertData) =>
-          alert?.alertinfoSet?.some((infoSet: AlertInfoSet) => {
-            const urgency = infoSet.urgency as string;
-            return urgency.toLowerCase() === selectedUrgency.toLowerCase();
-          })
+          alert?.alertinfoSet?.some(
+            (infoSet: AlertInfoSet) =>
+              infoSet.urgency?.toLowerCase() === selectedUrgency.toLowerCase()
+          )
         );
       }
 
       if (selectedSeverity !== "") {
         filteredData = filteredData.filter((alert: AlertData) =>
-          alert?.alertinfoSet?.some((infoSet: AlertInfoSet) => {
-            const severity = infoSet.severity as string;
-            return severity.toLowerCase() === selectedSeverity.toLowerCase();
-          })
+          alert?.alertinfoSet?.some(
+            (infoSet: AlertInfoSet) =>
+              infoSet.severity?.toLowerCase() === selectedSeverity.toLowerCase()
+          )
         );
       }
 
@@ -83,11 +110,10 @@ const MapComponentWithFilter: React.FC<MapComponentWithFilterProps> = ({
       ) {
         filteredData = filteredData.filter((alert: AlertData) =>
           alert?.alertinfoSet?.some((infoSet: AlertInfoSet) => {
-            let effectiveTimestamp =
+            const effectiveTimestamp =
               new Date(infoSet.effective as string).getTime() / 1000;
-            let expiresTimestamp =
-              new Date(infoSet.effective as string).getTime() / 1000;
-
+            const expiresTimestamp =
+              new Date(infoSet.expires as string).getTime() / 1000;
             if (
               selectedEffectiveDate &&
               selectedEffectiveDate[0] !== null &&
@@ -102,7 +128,7 @@ const MapComponentWithFilter: React.FC<MapComponentWithFilterProps> = ({
           })
         );
       }
-
+      console.log("filteredAlerts: ", filteredData);
       setFilteredAlerts(filteredData);
     }
   }, [
@@ -137,7 +163,7 @@ const MapComponentWithFilter: React.FC<MapComponentWithFilterProps> = ({
           >
             <Autocomplete
               disablePortal
-              id="combo-box-demo"
+              id="combo-box-urgency"
               options={urgencyOptions}
               getOptionLabel={(option) => option}
               sx={{
@@ -165,7 +191,7 @@ const MapComponentWithFilter: React.FC<MapComponentWithFilterProps> = ({
             />
             <Autocomplete
               disablePortal
-              id="combo-box-demo"
+              id="combo-box-severity"
               options={severityOptions}
               getOptionLabel={(option) => option}
               sx={{
@@ -196,13 +222,13 @@ const MapComponentWithFilter: React.FC<MapComponentWithFilterProps> = ({
               selectedDate={selectedEffectiveDate}
               setSelectedDate={setSelectedEffectiveDate}
             />
-          </Box>{" "}
+          </Box>
           <MapComponent
             mapContainerRef={mapContainerRef}
             mapRef={mapRef}
             alerts={filteredAlerts}
             countries={data?.listCountry}
-            boundingRegionCoordinates={boundingRegionCoordinates || undefined}
+            boundingRegionCoordinates={boundingRegionCoordinates}
             alertsLoading={alertsLoading}
             setAlertsLoading={setAlertsLoading}
           />
