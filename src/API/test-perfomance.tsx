c34d331747a4e6b6c5f9b4ad8f723cbceaff11f2
piraveenankirupakaran <piraveenankirupakaran@gmail.com>
2023-07-24 17:15:06 +0100

    :rotating_light: setup performance testing

---
+++ src/API/test-perfomance.tsx
@@ -0,0 +1,152 @@
+import { gql, useQuery } from "@apollo/client";
+import { CircularProgress } from "@mui/material";
+import { cap_aggregator } from "./API_Links";
+
+export const ALL_ALERTS_QUERY_1 = gql`
+  query MyQuery($regionId: String, $iso3: String, $continentId: String) {
+    listAlert(regionId: $regionId, iso3: $iso3, continentId: $continentId) {
+      status
+      source
+      sent
+      sender
+      references
+      scope
+      restriction
+      note
+      msgType
+      incidents
+      identifier
+      code
+      addresses
+      id
+      country {
+        id
+        name
+        region {
+          name
+        }
+      }
+      alertinfoSet {
+        web
+        urgency
+        audience
+        category
+        certainty
+        contact
+        effective
+        event
+        eventCode
+        headline
+        expires
+        instruction
+        language
+        onset
+        responseType
+        senderName
+        severity
+        id
+        description
+      }
+      sourceFeed {
+        url
+        name
+      }
+    }
+    listCountry {
+      id
+      iso3
+      multipolygon
+      name
+      polygon
+      centroid
+      region {
+        id
+        name
+        polygon
+      }
+    }
+  }
+`;
+
+export const ALL_ALERTS_QUERY_2 = gql`
+  query MyQuery($regionId: String, $iso3: String, $continentId: String) {
+    listAlert(regionId: $regionId, iso3: $iso3, continentId: $continentId) {
+      status
+      source
+      sent
+      sender
+      references
+      scope
+      restriction
+      note
+      msgType
+      incidents
+      identifier
+      code
+      addresses
+      id
+      country {
+        id
+        name
+        region {
+          name
+        }
+      }
+      alertinfoSet {
+        web
+        urgency
+        audience
+        category
+        certainty
+        contact
+        effective
+        event
+        eventCode
+        headline
+        expires
+        instruction
+        language
+        onset
+        responseType
+        senderName
+        severity
+        id
+        description
+      }
+      sourceFeed {
+        url
+        name
+      }
+    }
+  }
+`;
+
+export const ALL_COUNTRIES = gql`
+  query MyQuery {
+    listCountry {
+      centroid
+      id
+      iso3
+      multipolygon
+      name
+      polygon
+    }
+  }
+`;
+
+export default function TestPerformance() {
+  const { loading, error, data } = useQuery(ALL_COUNTRIES, {
+    client: cap_aggregator,
+    fetchPolicy: "no-cache",
+  });
+  return (
+    <>
+      {" "}
+      {loading && (
+        <CircularProgress sx={{ textAlign: "center" }} color="secondary" />
+      )}
+      {error && <p>Error: {error.message}</p>}
+      {!loading && !error && <h1>afafsafsa</h1>}
+    </>
+  );
+}
