b2f17e99617ae0b2d06b0b2ed8a752f39b7c13f6
Piraveenan Kirupakaran <piraveenankirupakaran@gmail.com>
2023-08-04 11:45:59 +0100

    :sparkles: Added Logic for districts and countries

--- src/components/MapComponent/TestMapComponentPopup2/TestMapComponentPopup2.tsx
+++ src/components/MapComponent/TestMapComponentPopup2/TestMapComponentPopup2.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl, { LngLatBoundsLike } from "mapbox-gl";
-import { Container, Box } from "@mui/material";
+import { Container, Box, Chip } from "@mui/material";
 import turfBbox from "@turf/bbox";
-import { UK_Polygon_Coordinates } from "./UnitedKingdom";
+import { Polygon_Coordinates } from "./UnitedKingdom";
 import { districts } from "./UnitedKingdomDistricts";
 
 const TestMapComponentPopup2 = () => {
   const mapContainerRef = useRef<HTMLDivElement | null>(null);
   const mapRef = useRef<mapboxgl.Map | null>(null);
-  const [isCountryPolygonClicked, setIsCountryPolygonClicked] = useState(false);
+  const [countryPolygonClicked, setCountryPolygonClicked] =
+    useState<string>("");
+
+  const [countrySelected, setCountrySelected] = useState(false);
 
   useEffect(() => {
     if (!mapRef.current) {
@@ -35,78 +38,94 @@ const TestMapComponentPopup2 = () => {
     if (!mapRef.current) return;
 
     mapRef.current.on("load", () => {
-      const sourceId = `polygon-source-1`;
-      const layerId = `polygon-layer-1`;
-      console.log(mapRef.current);
-      mapRef.current?.addSource(sourceId, {
-        type: "geojson",
+      Polygon_Coordinates.forEach((country, index) => {
+        const sourceId = `country-source-${country.ISO3}`;
+        console.log(sourceId);
 
-        data: {
-          type: "Feature",
-          geometry: {
-            type: "MultiPolygon",
-            coordinates: UK_Polygon_Coordinates,
+        const layerId = `country-layer-${country.ISO3}`;
+        mapRef.current?.addSource(sourceId, {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            geometry: {
+              type: country.type as any,
+              coordinates: country.coordinates as any,
+            },
+            properties: {},
           },
-          properties: {},
-        },
-      });
+        });
 
-      mapRef.current?.addLayer({
-        id: `${layerId}-border`,
-        type: "line",
-        source: sourceId,
-        paint: {
-          "line-color": "black",
-          "line-width": 2,
-        },
-      });
-      mapRef.current?.addLayer({
-        id: `${layerId}`,
-        type: "fill",
-        source: sourceId,
-        paint: {
-          "fill-color": "#ff0000",
-          "fill-opacity": 0.8,
-        },
-      });
+        mapRef.current?.addLayer({
+          id: `${layerId}-border`,
+          type: "line",
+          source: sourceId,
+          paint: {
+            "line-color": "black",
+            "line-width": 2,
+          },
+        });
+        mapRef.current?.addLayer({
+          id: `${layerId}`,
+          type: "fill",
+          source: sourceId,
+          paint: {
+            "fill-color":
+              countryPolygonClicked === country.ISO3 ? "#FFFFFF" : "#ff0000",
+            "fill-opacity": 0.8,
+          },
+        });
 
-      mapRef.current?.on(
-        "click",
-        layerId,
-        (e: mapboxgl.MapMouseEvent & mapboxgl.EventData) => {
-          if (!isCountryPolygonClicked) {
-            // Check if the layer has not been clicked yet
-            setIsCountryPolygonClicked(true); // Set the flag to true, indicating th
-            mapRef.current!.resize();
+        mapRef.current?.on(
+          "click",
+          layerId,
+          (e: mapboxgl.MapMouseEvent & mapboxgl.EventData) => {
+            setCountryPolygonClicked(country.ISO3);
+            setCountrySelected(true);
+            mapRef.current?.resize();
 
             const mapBoundingBox = turfBbox({
               type: "Feature",
               geometry: {
-                type: "MultiPolygon",
-                coordinates: UK_Polygon_Coordinates,
+                type: country.type as any,
+                coordinates: country.coordinates as any,
               },
             });
 
             const [minX, minY, maxX, maxY] = mapBoundingBox;
-            mapRef.current!.fitBounds(
+            mapRef.current?.fitBounds(
               [minX, minY, maxX, maxY] as LngLatBoundsLike,
               { padding: { top: 10, bottom: 25, left: 15, right: 5 } }
             );
 
-            mapRef.current?.setPaintProperty(
-              `polygon-layer-1`,
-              "fill-color",
-              "#FFFFFF"
+            mapRef.current?.setLayoutProperty(layerId, "visibility", "none");
+
+            Polygon_Coordinates.forEach((otherCountry) => {
+              if (otherCountry.ISO3 !== country.ISO3) {
+                mapRef.current?.setLayoutProperty(
+                  `country-layer-${otherCountry.ISO3}`,
+                  "visibility",
+                  "none"
+                );
+
+                mapRef.current?.setLayoutProperty(
+                  `country-layer-${otherCountry.ISO3}-border`,
+                  "visibility",
+                  "none"
+                );
+              }
+            });
+
+            // Add Districts
+            const filteredDistrictData = districts.find(
+              (data) => data.ISO3 === country.ISO3
             );
-
-            districts.splice(2, 3).forEach((district, index) => {
-              const districtSourceID = `district-source-${index}`;
-              const districtLayerID = `district-layer-${index}`;
-
+            filteredDistrictData?.districts.forEach((district, index) => {
+              const districtSourceID = `${sourceId}-district-${district.district_id}`;
+              const districtLayerID = `${layerId}-district-${district.district_id}`;
+              console.log(mapRef.current?.getSource(districtSourceID));
               if (!mapRef.current?.getSource(districtSourceID)) {
                 mapRef.current?.addSource(districtSourceID, {
                   type: "geojson",
-
                   data: {
                     type: "Feature",
                     geometry: {
@@ -116,14 +135,13 @@ const TestMapComponentPopup2 = () => {
                     properties: {},
                   },
                 });
-
                 mapRef.current?.addLayer({
                   id: `${districtLayerID}-border`,
                   type: "line",
                   source: districtSourceID,
                   paint: {
                     "line-color": "black",
-                    "line-width": 5,
+                    "line-width": 1.3,
                   },
                 });
                 mapRef.current?.addLayer({
@@ -131,8 +149,8 @@ const TestMapComponentPopup2 = () => {
                   type: "fill",
                   source: districtSourceID,
                   paint: {
-                    "fill-color": "#ff0000",
-                    "fill-opacity": 0.4,
+                    "fill-color": "#000000",
+                    "fill-opacity": 0.8,
                   },
                 });
 
@@ -141,12 +159,12 @@ const TestMapComponentPopup2 = () => {
                   type: "symbol",
                   source: districtSourceID,
                   layout: {
-                    "text-field": `${index}`, // Replace "District Name" with the actual name you want to display
-                    "text-font": ["Open Sans Bold"], // Use the bold font
+                    "text-field": `${index}`,
+                    "text-font": ["Open Sans Bold"],
                     "text-size": 12,
-                    "text-anchor": "center", // Center the text horizontally within the polygon
-                    "text-justify": "center", // Center the text horizontally within the polygon
-                    "text-offset": [0, 0], // Adjust the offset as needed to position the text vertically
+                    "text-anchor": "center",
+                    "text-justify": "center",
+                    "text-offset": [0, 0],
                   },
                   paint: {
                     "text-color": "#000000",
@@ -155,14 +173,58 @@ const TestMapComponentPopup2 = () => {
               }
             });
           }
-        }
-      );
+        );
+      });
     });
-  }, [isCountryPolygonClicked]);
+  }, [countryPolygonClicked]);
+  const countryControlChange = () => {
+    setCountrySelected(false);
+    Polygon_Coordinates.forEach((country) => {
+      const layerId = `country-layer-${country.ISO3}`;
+      const sourceId = `country-source-${country.ISO3}`;
 
+      const borderLayerId = `${layerId}-border`;
+
+      // Remove district layers
+      const filteredDistrictData = districts.find(
+        (data) => data.ISO3 === country.ISO3
+      );
+      filteredDistrictData?.districts.forEach((district, index) => {
+        const districtLayerId = `${layerId}-district-${district.district_id}`;
+        const districtSourceId = `${sourceId}-district-${district.district_id}`;
+        const districtBorderLayerId = `${districtLayerId}-border`;
+        const districtTextLayerId = `${districtLayerId}-text`;
+        mapRef.current?.getSource(districtSourceId) &&
+          mapRef.current?.removeSource(districtSourceId);
+        mapRef.current?.getLayer(districtLayerId) &&
+          mapRef.current?.removeLayer(districtLayerId);
+        mapRef.current?.getLayer(districtBorderLayerId) &&
+          mapRef.current?.removeLayer(districtBorderLayerId);
+        mapRef.current?.getLayer(districtTextLayerId) &&
+          mapRef.current?.removeLayer(districtTextLayerId);
+      });
+
+      mapRef.current?.setLayoutProperty(layerId, "visibility", "visible");
+      mapRef.current?.setLayoutProperty(borderLayerId, "visibility", "visible");
+    });
+
+    mapRef.current?.setCenter([0, 0]);
+    mapRef.current?.setZoom(1);
+  };
   return (
     <>
       <Container maxWidth="lg">
+        <Box sx={{ height: "20px", padding: "20px" }}>
+          {" "}
+          {countrySelected && (
+            <Chip
+              label="Country"
+              variant="outlined"
+              onDelete={countryControlChange}
+            />
+          )}
+        </Box>
+
         <Box sx={{ paddingTop: "30px", display: "flex" }}>
           <div
             ref={mapContainerRef}
