fe1e75b390c85e278c8f34bfc1b945a4769ca5ff
Lee <yizhouli1822220@gmail.com>
2023-08-14 11:50:05 +0100

    :zap: Modify the default lg width

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,6 +1,15 @@
 import { createTheme, Theme } from "@mui/material/styles";
 
 export const theme: Theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1280,
+      xl: 1536,
+    },
+  },
   palette: {
     primary: {
       main: "#FFFFFF",
