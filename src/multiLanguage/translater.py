03d8baa3b1aab2fa97e496de9fb6b3874a0c0c93
Piraveenan Kirupakaran <piraveenankirupakaran@gmail.com>
2023-08-20 17:40:12 +0100

    :sparkles: Updated Translator to allow inputs

--- src/multiLanguage/translater.py
+++ src/multiLanguage/translater.py
@@ -1,56 +1,74 @@
 import json
 from googletrans import Translator
-#pip3 install googletrans==3.1.0a0 
-def translate_values(values, target_language):
-    translator = Translator()
-    translations = [translator.translate(value, dest=target_language).text for value in values]
-    return translations
+import argparse
 
-target_language_code = 'es'
-output_filename = f"{target_language_code}.json"
-localesFolder = './locales/'
-languageCodes = './LanguageCodes/LanguageCodes.json'
-supportedLanguages = './locales/supportedLanguages.json'
+class TranslationManager:
+    def __init__(self, target_language_code):
+        self.target_language_code = target_language_code
+        self.translator = Translator()
 
-try:
-    with open(output_filename, 'r', encoding='utf-8') as output_file:
-        existing_translated_data = json.load(output_file)
-except FileNotFoundError:
-    existing_translated_data = {}
+    def translate_values(self, values):
+        translations = [self.translator.translate(value, dest=self.target_language_code).text for value in values]
+        return translations
 
-with open(localesFolder + 'en.json') as user_file:
-    input_data = json.load(user_file)
+    def translate_and_save(self, input_file, output_file, locales_folder, language_codes_file, supported_languages_file, overwrite=False):
+        try:
+            with open(str(locales_folder + output_file), 'r', encoding='utf-8') as output_file_obj:
+                existing_translated_data = json.load(output_file_obj)
+        except FileNotFoundError:
+            existing_translated_data = {}
+            overwrite=False
 
-values_to_translate = list(input_data.values())
+        with open(locales_folder + 'en.json') as user_file:
+            input_data = json.load(user_file)
 
-translated_values = translate_values(values_to_translate, target_language_code)
+        values_to_translate = list(input_data.values())
 
-for key, value in zip(input_data.keys(), translated_values):
-    if key not in existing_translated_data:
-        existing_translated_data[key] = value
+        translated_values = self.translate_values(values_to_translate)
 
-with open(localesFolder + output_filename, 'w', encoding='utf-8') as output_file:
-    json.dump(existing_translated_data, output_file,
-              ensure_ascii=False, indent=2)
+        for key, value in zip(input_data.keys(), translated_values):
+            if overwrite or key not in existing_translated_data:
+                existing_translated_data[key] = value
 
-print(f"Translated data saved to '{output_filename}'.")
+        with open(str(locales_folder + output_file), 'w', encoding='utf-8') as output_file_obj:
+            json.dump(existing_translated_data, output_file_obj, ensure_ascii=False, indent=2)
+
+        print(f"Translated data saved to '{output_file}'.")
+
+        with open(language_codes_file, 'r') as lang_codes_file:
+            language_codes = json.load(lang_codes_file)
+
+        language_entry = {"code": self.target_language_code, "name": language_codes[self.target_language_code]}
+
+        with open(supported_languages_file, 'r') as supported_lang_file:
+            supported_languages = json.load(supported_lang_file)
+
+        existing_codes = [item["code"] for item in supported_languages]
+        if self.target_language_code not in existing_codes:
+            supported_languages.append(language_entry)
+            with open(supported_languages_file, "w") as json_file:
+                json.dump(supported_languages, json_file, indent=4)
+        else:
+            print(f'Dictionary with code "{self.target_language_code}" already exists.')
 
 
-# Load LanguageCodes.json
-with open(languageCodes, 'r') as lang_codes_file:
-    language_codes = json.load(lang_codes_file)
+def main():
+    target_language_code = input("Enter the target language code (e.g., 'es' for Spanish): ")
+    overwrite_input = input("Do you want to overwrite existing translations? (y/n): ").strip().lower()
 
-a =   { "code": target_language_code, "name": language_codes[target_language_code] }
-print(a)
+    if overwrite_input == 'y':
+        overwrite = True
+    else:
+        overwrite = False
 
-with open(supportedLanguages, 'r') as supported_lang_file:
-    supported_languages = json.load(supported_lang_file)
+    output_filename = f"{target_language_code}.json"
+    localesFolder = './locales/'
+    languageCodes = './LanguageCodes/LanguageCodes.json'
+    supportedLanguages = './locales/supportedLanguages.json'
+
+    translator = TranslationManager(target_language_code)
+    translator.translate_and_save('en.json', output_filename, localesFolder, languageCodes, supportedLanguages, overwrite=overwrite)
 
 
-existing_codes = [item["code"] for item in supported_languages]
-if target_language_code not in existing_codes: 
-    supported_languages.append(a)
-    with open(supportedLanguages, "w") as json_file:
-        json.dump(supported_languages, json_file, indent=4)
-else:
-    print(f'Dictionary with code "{target_language_code}" already exists.')
+if __name__ == "__main__":
+    main()
