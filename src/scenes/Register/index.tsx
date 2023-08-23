0d3f8ceb20639a1d440b1a717b84a8b0464df176
Piraveenan Kirupakaran <piraveenankirupakaran@gmail.com>
2023-08-23 10:35:14 +0100

    Updated Register page

--- src/scenes/Register/index.tsx
+++ src/scenes/Register/index.tsx
@@ -234,7 +234,7 @@ const Register = () => {
 
             <PasswordComponent formik={formik} />
 
-            <Box
+            {/* <Box
               sx={{
                 display: "flex",
                 justifyContent: "center",
@@ -253,7 +253,7 @@ const Register = () => {
                   theme: "light",
                 }}
               />
-            </Box>
+            </Box> */}
             <Button
               type="submit"
               fullWidth
@@ -269,7 +269,7 @@ const Register = () => {
                 },
                 fontSize: "14px",
               }}
-              disabled={!formik.isValid || !formik.dirty || !isCaptchaVerified}
+              disabled={!formik.isValid || !formik.dirty}
             >
               Register
             </Button>
